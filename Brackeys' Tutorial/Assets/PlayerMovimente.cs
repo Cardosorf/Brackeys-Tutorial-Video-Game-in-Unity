ff5fa88dbabc2e8b50a066d15a931ce08e2d0c81
Rafael Cardoso <cardoso.rf@gmail.com>
2019-07-11 15:51:10 -0300

    +Player script (AddForce)
    Added a script that applies a force on the object.

---
+++ Brackeys' Tutorial/Assets/PlayerMovimente.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public class PlayerMovimente : MonoBehaviour
+{
+
+    // This is a reference to the RigidBody component called "rb"
+    public Rigidbody rb;
+     
+    // We marked this as "Fixed"Updated because we
+    // are using it to mess with physics.
+    void FixedUpdate()
+    {
+        rb.AddForce(0, 0, 2000 * Time.deltaTime); // Add a force of 2000 on the z-axis
+    }
+}
