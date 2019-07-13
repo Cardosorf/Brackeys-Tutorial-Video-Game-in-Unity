1dc94e04dba669025ba28c2d73773987b4e33009
Rafael Cardoso <cardoso.rf@gmail.com>
2019-07-13 10:40:55 -0300

    +Menu, Credits, Folder; #LevelComplete;
    Added prefab Folder.
    Fixed and issue that if the player had hit the end line ,but had collided with something before, it was showing the level completed panel.
    Added Menu and Credit panels.

---
+++ Brackeys' Tutorial/Assets/Scripts/EndTrigger.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class EndTrigger : MonoBehaviour
+{
+    public GameManager gameManager;
+
+    public PlayerCollision playerCollision;
+
+    void OnTriggerEnter(Collider other)
+    {
+
+        if(playerCollision.movement.enabled == true)
+        {
+            gameManager.CompleteLevel();
+        }
+
+    }
+}
